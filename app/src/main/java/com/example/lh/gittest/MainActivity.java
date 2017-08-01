b29a597f5a9284c269c144b192ef94832b19a0fd
第4个苹果 <745581093@qq.com>
2017-08-01 11:35:56 +0800

    feature分支

--- app/src/main/java/com/example/lh/gittest/MainActivity.java
+++ app/src/main/java/com/example/lh/gittest/MainActivity.java
@@ -5,12 +5,21 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tv_show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv_show= (TextView) findViewById(R.id.tv_show);
+        tv_show = (TextView) findViewById(R.id.tv_show);
+        setFeature3();
+    }
+    private void setText(){
         tv_show.setText("hello bug");
+    }
+    private void setFeature2(){
+        tv_show.setText("hello Feature2");
+    }
+    private void setFeature3(){
+        tv_show.setText("hello Feature3");
     }
 }
